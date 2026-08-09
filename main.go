64611133972c9d061394342170db38af405be8e5
gian5204 <gianagius502@gmail.com>
2026-08-09 10:09:27 +0200

    feat: add CLI argument parsing

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	err := addCommand(".env")
-	if err != nil {
-		fmt.Println(err)
-	}
+	if len(os.Args) < 2 {
+        fmt.Println("Usage: carry <command> [arguments]")
+        return
+    }
+
+	command := os.Args[1]
+
+    switch command {
+    case "add":
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: carry add <path>")
+			return
+		}
+
+		err := addCommand(os.Args[2])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+    default:
+        fmt.Printf("Unknown command: %s\n", command)
+    }
 }
