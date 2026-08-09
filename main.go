68014084f155c6d9ab453ec4015ea8cef19bc570
gian5204 <gianagius502@gmail.com>
2026-08-09 08:33:49 +0200

    feat: add Git repository detection

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Carry")
+	repo, err := detectRepo()
+	if err != nil {
+		fmt.Println("Error detecting git repository:", err)
+		return
+	}
+	fmt.Printf("Repo detected:\n%s\n", repo.Root)
 }
